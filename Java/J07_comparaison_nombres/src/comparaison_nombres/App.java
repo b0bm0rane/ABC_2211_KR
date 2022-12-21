dfbdef7e3c109a602db705b5a27f3b8c3fcb2075
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2022-12-21 09:57:53 +0100

    Màj inversion de 2 nombres

---
+++ Java/J07_comparaison_nombres/src/comparaison_nombres/App.java
@@ -0,0 +1,37 @@
+package comparaison_nombres;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// VARIABLES
+		
+		int a;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Comparaison de nombres");
+		
+		System.out.println("Veuillez saisir le nombre d'années");
+		
+		a = sc.nextInt();
+		
+		if (a<0)
+		
+		System.out.println("Vous n'êtes pas né");
+		
+		if (0>>=a=<<18)
+		
+		System.out.println("Vous êtes mineur");
+		
+		if (a>18)
+		
+		System.out.println("Vous êtes majeur"); 
+		
+		// FIN
+	}
+
+}
