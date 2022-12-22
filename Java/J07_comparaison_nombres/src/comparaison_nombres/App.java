0245cbb9f10d1d0da0ad207a03893ff265a87ef0
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2022-12-22 16:56:24 +0100

    Màj comparaison de nombres

--- Java/J07_comparaison_nombres/src/comparaison_nombres/App.java
+++ Java/J07_comparaison_nombres/src/comparaison_nombres/App.java
@@ -20,17 +20,17 @@ public class App {
 		a = sc.nextInt();
 		
 		if (a <= 0)
-		
-		System.out.println("Vous n'êtes pas né");
-		
-		if (a >= 18)
-		
-		System.out.println("Vous êtes majeur"); 
-		
-		if (a > 0 && a < 18);
-		
+		{
+			System.out.println("Vous n'êtes pas né");
+		}
+		else if (a >= 18)
+		{
+			System.out.println("Vous êtes majeur"); 
+		}
+		else
+		{
 		System.out.println("Vous êtes mineur");
-		
+		}
 		// FIN
 	}
 
