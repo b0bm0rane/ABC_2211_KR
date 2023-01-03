8e3aecddb0bd8972e14d49b63805beaabd9a149d
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-03 12:56:08 +0100

    Màj tri des nombres 2

--- J09_tri_nombres_2/src/tri_nombres_2/App.java
+++ J09_tri_nombres_2/src/tri_nombres_2/App.java
@@ -19,11 +19,15 @@ public class App {
 		
 		System.out.println("Tri de nombres 2");
 		
+		System.out.println();
+		
 		System.out.println("Veuillez saisir le nombre a");
 		
 		a = sc.nextDouble();
 		
 		System.out.println("Le nombre a est " + a);
+		
+		System.out.println();
 		
 		System.out.println("Veuillez saisir le nombre b");
 		
@@ -31,15 +35,60 @@ public class App {
 		
 		System.out.println("Le nombre b est " + b);
 		
+		System.out.println();
+		
 		System.out.println("Veuillez saisir le nombre c");
 		
 		c = sc.nextDouble();
 		
 		System.out.println("Le nombre c est " + c);
 		
-		if (a<b&b<c);
+		System.out.println();
 		
-		System.out.println(a + " < " + b + " < " + c);
+		if (a<b&&b<c) {
+		
+			System.out.println("a<b<c");
+		
+			System.out.println(a + " < " + b + " < " + c);
+		}
+		
+		else if (a<c&&c<b) {
+		
+			System.out.println("a<c<b");
+		
+			System.out.println(a + " < " + c + " < " + b);
+		}
+		
+		else if (b<a&&a<c) {
+		
+			System.out.println("b<a<c");
+		
+			System.out.println(b + " < " + a + " < " + c);
+		}
+		
+		else if (b<c&&c<a) {
+		
+			System.out.println("b<c<a");
+		
+			System.out.println(b + " < " + c + " < " + a);
+		}
+			
+		else if (c<a&&a<b) {
+		
+			System.out.println("c<a<b");
+		
+			System.out.println(c + " < " + a + " < " + b);
+		}
+			
+		else if (c<b&&b<a) {
+		
+			System.out.println("c<b<a");
+		
+			System.out.println(c + " < " + b + " < " + a);
+		}
+		
+		sc.close();
+		
 	}
 
 }
