fa33362e3888c477f0d526be317ad19ea939db77
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-04 11:58:06 +0100

    Màj année bissextile

--- Java/J10_année_bissextile/src/année_bissextile/App.java
+++ Java/J10_année_bissextile/src/année_bissextile/App.java
@@ -1,10 +1,30 @@
 package année_bissextile;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// VARIABLES
+		
+		int nj;
+		
+		Scanner sc = new Scanner (System.in);
+		
+		// Début
+		
+		System.out.println("Année bissextile");
+		
+		System.out.println("Entrer le nombre de jours dans l'année");
+		
+		nj = sc.nextInt();
+		
+		System.out.println("L'année comporte " + nj + " jours");
+		
+		if
+		
 	}
 
 }
