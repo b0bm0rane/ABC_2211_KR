bfeb7946de98100d4e89bad60e6fea1c8fec79e8
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-04 11:40:26 +0100

    Ajout année bissextile

---
+++ Java/J10_année_bissextile/src/année_bissextile/App.java
@@ -0,0 +1,10 @@
+package année_bissextile;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/J10_année_bissextile/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KRoussotte
+ *
+ */
+module J10_année_bissextile {
+}
