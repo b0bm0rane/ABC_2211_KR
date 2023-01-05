9d9b79e2af1b0a6d7b79e6ede80732f2c8acbce6
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-05 12:54:03 +0100

    Màj diviseurs

--- Java/J12_nombre_premier/src/nombre_premier/App.java
+++ Java/J12_nombre_premier/src/nombre_premier/App.java
@@ -1,4 +1,4 @@
-package diviseurs_nombre;
+package nombre_premier;
 
 import java.util.Scanner;
 
@@ -7,28 +7,24 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// DÉBUT
+		
 		// VARIABLES
 		
 		int n;
 		
 		Scanner sc = new Scanner (System.in);
 		
-		// DÉBUT
-		
-		System.out.println("Diviseurs d'un nombre");
-		
-		System.out.println("Entrer un nombre entier");
+		System.out.println("Entrer un nombre");
 		
 		n = sc.nextInt();
 		
-		for (int i=2;i<n;i++) {
-		
-		if (n%i==0) {
-		
-		System.out.println(i);
-			}
+		if (n%1==0&&n%n==0) {
+			
+		System.out.println(n + " est un nombre premier");
 		
 		}
+		
 	}
 
 }
