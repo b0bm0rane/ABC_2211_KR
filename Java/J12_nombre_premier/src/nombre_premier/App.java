3d63330117697bd483eef8a73cdd050beebe305d
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-05 16:55:03 +0100

    Màj nombre premier

--- Java/J12_nombre_premier/src/nombre_premier/App.java
+++ Java/J12_nombre_premier/src/nombre_premier/App.java
@@ -12,6 +12,8 @@ public class App {
 		// VARIABLES
 		
 		int n;
+		int i;
+		int nd;
 		
 		Scanner sc = new Scanner (System.in);
 		
@@ -19,9 +21,16 @@ public class App {
 		
 		n = sc.nextInt();
 		
-		if (n%1==0&&n%n==0) {
-			
-		System.out.println(n + " est un nombre premier");
+		for (i=2;i<n-1;i++) {
+		
+		if (n%i==0) {
+			System.out.println(n + " n'est pas un nombre premier");
+		}
+		else {
+		
+			System.out.println(n + " est un nombre premier");
+		
+		}
 		
 		}
 		
