69e710718a0893e1987a134dd5dceee0c2074a63
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-16 15:47:12 +0100

    Màj boucles

--- Java/J19_boucles/src/boucles/App.java
+++ Java/J19_boucles/src/boucles/App.java
@@ -2,9 +2,41 @@ package boucles;
 
 public class App {
 
+	/**
+	 * afficher tous les nombres de 1 à 100
+	 * en remplçant les multiples de 7 par "bzz"
+	 * et les multiples de 5 par "biz"
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int indice = 1;
+		
+		while(indice <= 100) {
+			
+			if(indice % 7 == 0) {
+				
+				System.out.println("bzz");
+			}
+			
+			else if(indice % 5 == 0) {
+				
+				System.out.println("biz");
+				
+			}
+			
+			else {
+				
+				System.out.println(indice);
+				
+			}
+
+			indice++;
+
+		}
+		
 	}
 
 }
