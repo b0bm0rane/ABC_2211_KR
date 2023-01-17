7820295916e9cab728a05992b9705935a4438a6b
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-17 12:21:30 +0100

    Ajout boucles

---
+++ Java/J20_tableaux/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KRoussotte
+ *
+ */
+module J20_tableaux {
+}
---
+++ Java/J20_tableaux/src/tableaaux/App.java
@@ -0,0 +1,18 @@
+package tableaaux;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] tableau0 = {"bleu", "rouge", "vert", "jaune", "noir"};
+		
+		System.out.println(tableau0[4]);
+		
+		int [] tableau1 = {1, 2, 3, 4, 5};
+		
+		System.out.println(tableau1 [4]);
+		
+		}
+		
+}
