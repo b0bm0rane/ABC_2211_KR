a34a028584d24ab7d53379d29138803dfe276999
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-26 15:54:59 +0100

    Ajout jour semaine

--- Java/J28_fonction_jour_semaine/src/fonction_jour_semaine/App.java
+++ Java/J28_fonction_jour_semaine/src/fonction_jour_semaine/App.java
@@ -1,7 +1,9 @@
 package fonction_jour_semaine;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
