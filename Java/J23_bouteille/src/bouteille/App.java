724bd36320e38acb86ecb51a284456354543d9d0
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-01-26 09:35:09 +0100

    màj bouteille

--- Java/J23_bouteille/src/bouteille/App.java
+++ Java/J23_bouteille/src/bouteille/App.java
@@ -5,6 +5,20 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Bouteille maGirafe = new Bouteille("verre", 3, true, true);
+		
+		maGirafe.ouvrir();
+		
+		maGirafe.vider();
+		
+		System.out.println("j'ai la tete dans le caniveau je vomis et retourne au bar");
+	
+		maGirafe.remplir();
+		
+		maGirafe.vider();
+		
+		System.out.println("retour au caniveau et c'est reparti pour un tour");
+		
 	}
 
 }
---
+++ Java/J23_bouteille/src/bouteille/Bouteille.java
@@ -0,0 +1,67 @@
+package bouteille;
+
+public class Bouteille {
+
+	// états / attributs 
+	
+	private String matiere; // état inaccessible de l'exterieur de la classe
+	
+	private float volumeEnL;
+	
+	private Boolean gazeux;
+	
+	private Boolean ouverte;
+	
+	private Boolean alcool;
+	
+	private Boolean remplie;
+	
+	public Bouteille(String mat, float vol, Boolean gaz, Boolean alc) {
+		
+		this.matiere = mat;
+		
+		this.volumeEnL = vol;
+
+		this.gazeux = gaz;
+		
+		this.ouverte = false; // sinon plus de bulles xD
+		
+		this.alcool = alc;
+		
+		this.remplie = true;
+		
+	}
+	
+	public void ouvrir() {
+		
+		this.ouverte = true;
+		
+		System.out.println("j'ai ouvert ma bouteille");
+		
+	}
+	
+	public void fermer() {
+		
+		this.ouverte = false;
+		
+		System.out.println("ma bouteille est fermee");
+		
+	}
+	
+	public void vider() {
+		
+		this.remplie = false;
+		
+		System.out.println("la bouteille est vide");
+		
+	}
+	
+	public void remplir() {
+		
+		this.remplie = true;
+		
+		System.out.println("la bouteille est remplie");
+	}
+	
+}
+
