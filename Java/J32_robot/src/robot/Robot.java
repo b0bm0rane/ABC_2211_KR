62eb1f443d67a34cd6d13ed0dda40b65b5cf4262
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-02-07 16:57:53 +0100

    Màj robot

--- Java/J32_robot/src/robot/Robot.java
+++ Java/J32_robot/src/robot/Robot.java
@@ -2,6 +2,20 @@ package robot;
 
 public class Robot {
 
+	private boolean allume;
 	
+	private boolean charge;
+	
+	private boolean branche;
+	
+	public Robot (boolean allume, boolean charge, boolean branche) {
+		
+		this.allume = allume;
+		
+		this.charge = charge;
+		
+		this.branche = branche;
+		
+	}
 	
 }
