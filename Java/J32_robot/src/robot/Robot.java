73e41c8e1db783e6afbc004d983d1c7eab29201c
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-02-10 11:50:21 +0100

    Màj robot

--- Java/J32_robot/src/robot/Robot.java
+++ Java/J32_robot/src/robot/Robot.java
@@ -4,20 +4,96 @@ public class Robot {
 
 	private boolean allume;
 	
-	private boolean charge;
+	private int niveauCharge;
 	
 	private boolean branche;
 	
-	public Robot (boolean allume, boolean charge, boolean branche) {
+	private String nom;
+	
+	private String miaou;
+
+	private String ronron;
+	
+	private boolean queue;
+	
+	public Robot (boolean allume, int niveauCharge, boolean branche, String nom, String miaou, String ronron, boolean queue) {
 		
 		this.allume = allume;
 		
-		this.charge = charge;
+		this.niveauCharge = niveauCharge;
 		
 		this.branche = branche;
 		
+		this.nom = nom;
 		
+		this.miaou = "Miaou !, miaouuuuu !!, miaouuuuuuuuuu !!!";
 		
-	}
+		this.ronron = "Rrrrrrrrrr ... ";
+		
+		this.queue = queue;
+		
+		}
 	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public boolean allumer() {
+		
+		if (!this.allume) {
+			
+			if (this.niveauCharge > 0 || this.branche) {
+				
+				this.allume = true;
+			
+				return true;
+		
+			}
+				
+			else {
+				
+				return false;
+				
+			}
+	
+		}
+		
+		else {
+			
+				return false;
+		
+		}	
+	
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+public boolean eteindre() {
+		
+		if (this.allume) {
+			
+				this.allume = false;
+			
+				return true;
+		
+			}
+				
+		else {
+				
+				return false;
+	
+		}		
+				
+	}			
+				
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public boolean miauler() {
+	
+		if (this.allume) {
+			
+			
+			
+		}
+	
+	}
+
 }
